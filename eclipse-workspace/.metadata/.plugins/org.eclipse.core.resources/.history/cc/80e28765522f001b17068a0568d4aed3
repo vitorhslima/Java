540eb490c068e89616dfbd83abadb327d6c011e9
vitorhslima <vitorhsouzalima@gmail.com>
2020-11-25 18:08:06 -0300

    Leitura com java.IO

---
+++ eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.history/cc/80e28765522f001b17068a0568d4aed3
@@ -0,0 +1,32 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscritaFileWriter {
+
+	public static void main(String[] args) throws IOException {
+
+		//Fluxo de entrada com um arquivo
+		
+//		OutputStream fos = new FileOutputStream("metodosVistos1.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+		FileWriter fw = new FileWriter("metodosVistos1.txt");		
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("Ganha novas armas funcionais, compete pela gloria no recem-aberto estadio em Warzone");
+		bw.newLine();		
+		bw.write("desbloqueia Skins de Operacional gratuitas, diagramas de armas, por regioes e tokens de XP");
+		bw.newLine();
+		bw.write("e prepara-te para a viagem da tua vida num comboio em movimento em Verdansk.");
+		
+		bw.close(); // to close Buffer
+	}
+
+}
---
+++ eclipse-workspace/java-io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,42 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.Socket;
+
+public class TesteCopiarArquivo {
+
+	public static void main(String[] args) throws IOException {
+
+		//Fluxo de entrada com um arquivo
+		
+		Socket s = new Socket();
+		
+		InputStream fis = s.getInputStream();   //System.in; //new FileInputStream("metodosVistos.txt");
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream fos = s.getOutputStream(); //System.out; //new FileOutputStream("metodosVistos1.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha =  br.readLine();
+		
+		while(linha != null && !linha.isEmpty()) {
+			bw.write(linha);
+			bw.newLine();
+			linha = br.readLine();
+		}		
+		
+		br.close(); // to close Buffer
+		bw.close();
+	}
+
+}
